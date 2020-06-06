0663ff40282d23dd25084815f0ae6fe1f7a6f1df
Francisco <f.lamego0009@gmail.com>
2020-06-06 18:39:18 +0100

    Update robots
    Houve um erro com o estabelecimento do robot que ja tinha feito anteriormente. Esta é a estrutura apropriada de trabalho a realizar daqui para a frente até a elaboração total do trabalho.

---
+++ src/main/java/mark_projects/MARK2.java
@@ -0,0 +1,10 @@
+package mark_projects;
+
+import robocode.AdvancedRobot;
+
+/**
+ * Esta é a classe onde se irá desenvolver o robot com a capacidade de disparo autonomo
+ * Implementacao do codigo com base em machine learning e utilizacao do H2O
+ */
+public class MARK2 extends AdvancedRobot {
+}
