608aced2f9f22d4ec91b3e268ebd1e8fb196b527
Francisco <f.lamego0009@gmail.com>
2020-06-12 18:19:57 +0100

    reproducePopulation.java
    implementation of further functions

--- src/main/java/mark_projects/GeneticAlgorithm.java
+++ src/main/java/mark_projects/GeneticAlgorithm.java
@@ -7,9 +7,8 @@ import robocode.AdvancedRobot;
 
 import java.awt.*;
 import java.awt.geom.Line2D;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
-import java.util.Random;
 
 /**
  * Esta irá ser a classe utilizada para a construcao do algoritmo genético que vai ser utilizado nos
@@ -17,14 +16,23 @@ import java.util.Random;
  */
 public class GeneticAlgorithm extends AdvancedRobot{
 
-    private static boolean flag = false;
+    private static ArrayList<List<IPoint>> pointsList = new ArrayList<>(); //lista de listas de pontos
+    private static List<IPoint> finalPointList = null; //lista final onde vai guardado o melhor caminho a seguir
 
+    /**
+     * Funcao responsavel por determinar um caminho valido a percorrer pelo robot desde o ponto de partida ao ponto de chegada
+     * Algoritmo genetico determina o caminho valido mais curto encontrado entre 2 pontos usando tecnicas de cruzamento, selecao
+     * e mutacao genetica
+     * @param populationSize quantidade de pontos maximo existente no caminho a percorrer
+     * @param maxIterations numero de iteracoes maxima permitida para o algoritmo gerar novas populacoes
+     * @param mutationRate taxa de mutacao aplicavel ao melhor caminho encontrado
+     * @param conf configuracao inicial do mapa carregado (dimensao, obstaculos)
+     * @return retorna o melhor caminho calculado a ser percorrido pelo robot
+     */
     public static List<IPoint> markGeneticAlgorithm(int populationSize , int maxIterations , double mutationRate , UIConfiguration conf) {
-        int i = 0 , a = 0;
-        double index, prevIndex = 0;
-        List<IPoint> finalPointList = null; //lista final onde vai guardado o melhor caminho a seguir
+        int i = 0 , a = 1;
+        double index, prevIndex = Integer.MIN_VALUE;
         List<IPoint> points = new ArrayList<>(); //lista temporária onde vai ser carregado novas populacoes a cada iteracao
-        List<Rectangle> obstacles = conf.getObstacles(); //lista com os obstáculos encontrados no mapa
         Random rand = new Random();
 
         do{
@@ -38,69 +46,58 @@ public class GeneticAlgorithm extends AdvancedRobot{
                 int y2Value = rand.nextInt(conf.getHeight() - 20);
                 y2Value += 20;
 
-                Line2D line1 = new Line2D.Double(x1Value,y1Value,x2Value,y2Value);
-
                 //se o ponto onde nos encontramos for o ultimo ponto entao adiciona esse como o ponto seleccionado pelo utilizador
                 if(a == populationSize - 1){
-                    line1 = new Line2D.Double(x1Value,y1Value,conf.getEnd().getX(),conf.getEnd().getY());
                     x2Value = conf.getEnd().getX();
                     y2Value = conf.getEnd().getY();
                 }
-
-                //determinar se a linha se intercepta com algum dos obstáculos
-                for(int c = 0 ; c < obstacles.size() ; c++){
-                    if(line1.intersects(obstacles.get(c).getBounds2D())){
-                        flag = true;
-                    }
-                }
                 points.add(new Point(x2Value , y2Value));
-                x1Value = x2Value;
-                y1Value = y2Value;
                 a++;
             } while(a < populationSize);
-            a = 0;
-            index = getFitness(points);
+            a = 1;
+            index = getFitness(points , conf);
 
             /**
              * Se o index de fitness registado nesta populacao for maior do que o anterior, entao esta populacao vai
              * ser a seleccionada caso contrário continua com a populacao anterior, ou seja, nao faz nada!
+             * 1º selecao feita aqui:
              */
             if(index > prevIndex){
                 finalPointList = points;
                 prevIndex = index;
             }
+            pointsList.add(points); //guardar todos os caminhos gerados (podem ser validos ou inválidos)
             points = new ArrayList<>();
             i++;
         } while (i < maxIterations);
 
-        mutateList(finalPointList , mutationRate);
+        reproducePopulation(pointsList , conf); //2º selecao feita aqui
+        //mutateList(finalPointList , mutationRate); //mutacao da melhor selecao feita aqui
+        System.out.println("Fitness final: " + getFitness(finalPointList , conf));
         return finalPointList;
     }
 
     /**
      * Funcao responsavel por determinar o nivel de fitness de um caminho escolhido pelo algoritmo genético
      * @param points escala quantificavel de pontos atribuidos a cada caminho (mais pontos significa melhor fitness)
+     * @param conf configuracao do mapa carregado
      * @return quantidade de pontos
      */
-    public static double getFitness(List<IPoint> points){
+    public static double getFitness(List<IPoint> points , UIConfiguration conf){
         //se o caminho resulta num caminho que nao passa por cima de nenhuma posicao inimiga, entao ganha 10000 pontos de fitness automáticamente.
         double fitnessPoints = 0;
-        if(points != null){
-            if(flag == false){
-                fitnessPoints += 10000;
-            }
-            flag = false;
-            double distance = 0;
+        double distance = 0;
 
-            for(int a = 0 ; a < points.size() - 1 ; a++){
-                distance += getDistanceBetweenPoints(points.get(a) , points.get(a + 1));
-            }
+        if(collisionDetection(points , conf) == false){
+            fitnessPoints = 10000;
+        }
+        distance = 0;
 
-            fitnessPoints -= distance;
+        for(int a = 0 ; a < points.size() - 1 ; a++){
+            distance += getDistanceBetweenPoints(points.get(a) , points.get(a + 1));
         }
-        else {
-            fitnessPoints = 0;
-        }
+
+        fitnessPoints -= distance;
         /*
         Se for feito com o modelo de populacao generational significa que metemos aqui todos os pontos de um unico caminho
         para ver qual o seu fitness, pois a seguir, como estamos a utilizar o modelo generational, toda essa populacao vai ser
@@ -120,9 +117,9 @@ public class GeneticAlgorithm extends AdvancedRobot{
         int y1 = point1.getY();
         int x2 = point2.getX();
         int y2 = point2.getY();
-        double distance = 0;
-        double yAxis = 0;
-        double xAxis = 0;
+        double distance;
+        double yAxis;
+        double xAxis;
 
         if(x1 > x2 && y1 > y2){ //↙
             yAxis = y1 - y2;
@@ -144,11 +141,86 @@ public class GeneticAlgorithm extends AdvancedRobot{
         return distance;
     }
 
+    /**
+     * Funcao responsavel por retornar uma lista inicialmente válida, noutra lista tambem válida mas com um fitness superior
+     * a lista de pontos que entrou como parametro.
+     * @param list a lista válida original
+     * @param mutationRate  taxa de mutacao a aplicar a um ponto aleatorio na lista
+     * @return uma lista válida mutada se possivel. retorna a lista original se impossivel.
+     */
     public static List<IPoint> mutateList(List<IPoint> list , double mutationRate){
         /*
-        a ideia aqui é ir a cada ponto do caminho ja escolhido como valido e mais curto e modificar os seus pontos
-        de X e Y um a um e ver se o seu fitness aumenta!
+        ir a 1 ponto aleatorio do melhor caminho selecionado e modificar a sua posicao em +/- conf.get(pos).getXouY();
          */
         return list;
+    }
+
+    /**
+     * Funcao responsavel por cruzar uma lista de caminhos carregados na funcao markGeneticAlgorithm.java
+     * @param pointsList lista de pontos a ser carregada
+     * @param conf configuracao inicial do mapa carregado (dimensao, obstaculos)
+     */
+    public static void reproducePopulation(ArrayList<List<IPoint>> pointsList , UIConfiguration conf){
+        Random rand = new Random();
+        System.out.println("Melhor fitness registado até ao momento: " + getFitness(finalPointList , conf));
+        System.out.println("Pontos da lista: " + finalPointList.toString() + "\n\n\n");
+
+        for(int a = 0 ; a < pointsList.size() - 1 ; a = a + 2){
+            int point1 = rand.nextInt((pointsList.get(0).size() - 1) - 1) + 1;
+            int point2 = point1;
+
+            while (point2 == point1){
+                point2 = rand.nextInt((pointsList.get(0).size() - 1) - 1) + 1;
+            }
+            IPoint p1 = pointsList.get(a).get(point1);
+            IPoint p2 = pointsList.get(a).get(point2);
+
+            pointsList.get(a).set(point1 , pointsList.get(a + 1).get(point1));
+            pointsList.get(a).set(point2 , pointsList.get(a + 1).get(point2));
+            pointsList.get(a + 1).set(point1 , p1);
+            pointsList.get(a + 1).set(point2 , p2);
+            System.out.println("Fitness da lista original depois de passar pelo cruzamento: " + getFitness(finalPointList , conf));
+            System.out.println("Pontos da lista agora: " + finalPointList.toString());
+        }
+
+        for(int b = 0 ; b < pointsList.size() ; b++){
+            System.out.println(getFitness(finalPointList , conf));
+            double score = getFitness(pointsList.get(b) , conf);
+            if(score > getFitness(finalPointList , conf)){
+                System.out.println("Score atual: " + score + " > " + "Score anterior: " + getFitness(finalPointList , conf));
+                finalPointList = pointsList.get(b);
+                System.out.println("Lista atualizada para uma melhor solucao de cruzamentos com um fitness de: " + score);
+            }
+        }
+    }
+
+    /**
+     * Funcao responsavel por determinar se um caminho passado por parametro cruza algum obstaculo do mapa
+     * @param pointList a lista a ser passada por parametro
+     * @param conf configuracao inicial do mapa (dimensao, obstaculos)
+     * @return retorna true se passa por algum obstaculo, false caso contrario
+     */
+    public static boolean collisionDetection(List<IPoint> pointList , UIConfiguration conf){
+        List<Rectangle> obstacles = conf.getObstacles(); //lista com os obstáculos encontrados no mapa
+        int count = 0;
+
+        for(int a = 0 ; a < pointList.size()-1 ; a++){
+            int x1 = pointList.get(a).getX();
+            int y1 = pointList.get(a).getY();
+            int x2 = pointList.get(a+1).getX();
+            int y2 = pointList.get(a+1).getY();
+            Line2D line1 = new Line2D.Double(x1,y1,x2,y2);
+
+            //determinar se a linha se intercepta com algum dos obstáculos
+            for(int c = 0 ; c < obstacles.size() ; c++){
+                if(line1.intersects(obstacles.get(c).getBounds2D())){
+                    count++;
+                }
+            }
+        }
+        if (count > 0)
+            return true;
+        else
+            return false;
     }
 }
--- src/main/java/samples/PathDrawingSample.java
+++ src/main/java/samples/PathDrawingSample.java
@@ -1,14 +1,12 @@
 package samples;
 
 import impl.UIConfiguration;
+import interf.IPoint;
 import interf.IUIConfiguration;
 import maps.Maps;
 import mark_projects.GeneticAlgorithm;
 import viewer.PathViewer;
-import impl.Point;
-import interf.IPoint;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,41 +16,14 @@ public class PathDrawingSample {
     public static IUIConfiguration conf;
 
     public static void main(String[] args) {
-
         try {
             conf = Maps.getMap(1);
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
-
-        List<IPoint> points = new ArrayList<>();
-        points = GeneticAlgorithm.markGeneticAlgorithm(3 , 2000 , 0.5 , (UIConfiguration) conf);
-        /*
-        points.add(conf.getStart());
-        points.add(new Point(200, 200));
-        points.add(new Point(250, 500));
-        points.add(new Point(300, 350));
-        points.add(conf.getEnd());
-         */
-
+        List<IPoint> points;
+        points = GeneticAlgorithm.markGeneticAlgorithm(7 , 100 , 0.5 , (UIConfiguration) conf);
         PathViewer pv = new PathViewer(conf);
-
-        pv.setFitness(9999);
-        pv.setStringPath("(ponto1, ponto2, bla bla bla...)");
-
-        //quando utilizado dentro de um ciclo permite ir atualizando o desenho e ver o algoritmo a progredir
-        //por exemplo: desenhar o melhor caminho de cada geração
-        /*
         pv.paintPath(points);
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            System.err.println(e.getMessage());
-        }
-        points.add(new Point(500, 500));
-        points.add(new Point(600, 700));
-         */
-        pv.paintPath(points);
-
     }
 }
